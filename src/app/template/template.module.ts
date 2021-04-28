caa40c38b54e3e668ea6c90d6605b2c72b3afb28
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-28 15:03:46 +0200

    ADD basicos component html

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { BasicosComponent } from './basicos/basicos.component';
 
-
 @NgModule({
-  declarations: [
-    SwitchesComponent,
-    DinamicosComponent,
-    BasicosComponent
-  ],
-  imports: [
-    CommonModule,
-    TemplateRoutingModule
-  ]
+  declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
+  imports: [CommonModule, TemplateRoutingModule, FormsModule],
 })
-export class TemplateModule { }
+export class TemplateModule {}
