28864cc086f2af34c483b7b770012d7ac97ffbac
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-28 13:43:18 +0200

    ADD routing to template and reactive

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
   declarations: [
-    BasicosComponent
+    BasicosComponent,
+    DinamicosComponent,
+    SwitchesComponent
   ],
   imports: [
     CommonModule,
