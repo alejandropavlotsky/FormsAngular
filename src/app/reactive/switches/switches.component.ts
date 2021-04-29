8163d1f441dde53192a20831e501e101d830ce8b
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-29 12:48:42 +0200

    ADD swithces reactive form

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,16 +1,44 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue],
+  });
 
-  constructor() { }
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.miFormulario.reset({ ...this.persona, condiciones: false });
+
+    // this.miFormulario
+    //   .get('condiciones')
+    //   ?.valueChanges.subscribe((condiciones) => {
+    //     console.log(condiciones);
+    //   });
+
+    this.miFormulario.valueChanges.subscribe(({ condiciones, ...rest }) => {
+      // delete form.condiciones;
+      this.persona = rest;
+    });
   }
 
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+    this.persona = formValue;
+    console.log(formValue);
+  }
 }
