2bde0cb0ceefa69eb22944918e6ab66f714e4b2a
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-29 13:14:23 +0200

    CREATE login and registro component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
-
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegistroComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginComponent, RegistroComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
