4f8393f79ceeb5986bc46637065b4e7b7167014b
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-29 14:57:40 +0200

    ADD validaciones service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,6 +8,6 @@ import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [LoginComponent, RegistroComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,16 +1,45 @@
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-
+import { emailPattern } from '../../../shared/validator/validaciones';
+import { ValidatorService } from '../../../shared/validator/validator.service';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegistroComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [
+      '',
+      [Validators.required, Validators.pattern(this.vs.nombreApellidoPattern)],
+    ],
+    email: [
+      '',
+      [Validators.required, Validators.pattern(this.vs.emailPattern)],
+    ],
+    username: ['', [Validators.required, this.vs.noPuedeSerStrider]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private vs: ValidatorService) {}
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Alejandro Murawczik',
+      email: 'test1@test.com',
+      username: 'Alexepa',
+    });
   }
 
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+    this.miFormulario.markAllAsTouched;
+    this.miFormulario.reset();
+  }
 }
